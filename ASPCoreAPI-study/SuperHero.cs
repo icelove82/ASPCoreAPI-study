90cb2ab5aba350fb04e287e9e0a7114c46e3bc78
YUN <yunmyeonghun@gmail.com>
2022-02-20 12:18:26 +0900

    study 02 Add a new model

---
+++ ASPCoreAPI-study/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPCoreAPI_study
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Place { get; set; } = String.Empty;
+    }
+}
