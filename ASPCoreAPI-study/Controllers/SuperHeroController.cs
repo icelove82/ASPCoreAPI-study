3bcfdfbbac3ab26bb74b17ee760faa9b47cc54ec
YUN <yunmyeonghun@gmail.com>
2022-02-21 13:47:27 +0900

    study 03 Add a new controller

---
+++ ASPCoreAPI-study/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreAPI_study.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heros = new List<SuperHero>()
+            {
+                new SuperHero{
+                    Id=1,
+                    Name="Spider Man",
+                    FirstName="Peter",
+                    LastName="Parker",
+                    Place="NewYork City"
+                }
+            };
+
+            return Ok(heros);
+        }
+    }
+}
