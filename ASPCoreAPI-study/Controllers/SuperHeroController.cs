d8bc67b1f87d625a1305dab9ea316eae5e82a1f2
YUN <yunmyeonghun@gmail.com>
2022-02-22 17:55:03 +0900

    study 05 Add a post route

--- ASPCoreAPI-study/Controllers/SuperHeroController.cs
+++ ASPCoreAPI-study/Controllers/SuperHeroController.cs
@@ -7,21 +7,29 @@ namespace ASPCoreAPI_study.Controllers
     [Route("api/[controller]")]
     public class SuperHeroController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<SuperHero>>> Get()
+        private List<SuperHero> heros = new List<SuperHero>
         {
-            var heros = new List<SuperHero>
-            {
-                new SuperHero{
-                    Id=1,
-                    Name="Spider Man",
-                    FirstName="Peter",
-                    LastName="Parker",
-                    Place="NewYork City"
-                }
-            };
+            new SuperHero{
+                Id=1,
+                Name="Spider Man",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="NewYork City"
+            }
+        };
 
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetHero()
+        {
             return Ok(heros);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heros.Add(hero);
+            return Ok(heros);
+        }
+
     }
 }
