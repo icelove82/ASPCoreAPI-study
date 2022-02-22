f3bd627b0880b5e709b9d1ff13e365b693d90c7d
YUN <yunmyeonghun@gmail.com>
2022-02-22 18:05:00 +0900

    study 06 Add a get single hero API

--- ASPCoreAPI-study/Controllers/SuperHeroController.cs
+++ ASPCoreAPI-study/Controllers/SuperHeroController.cs
@@ -15,13 +15,32 @@ namespace ASPCoreAPI_study.Controllers
                 FirstName="Peter",
                 LastName="Parker",
                 Place="NewYork City"
+            },
+            new SuperHero{
+                Id=2,
+                Name="Ironman",
+                FirstName="Tony",
+                LastName="Stark",
+                Place="Long Island"
             }
         };
 
         [HttpGet]
-        public async Task<ActionResult<List<SuperHero>>> GetHero()
+        public async Task<ActionResult<List<SuperHero>>> GetHeros()
         {
             return Ok(heros);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            var hero = heros.Find(it => it.Id == id);
+
+            if (hero == null)
+                return BadRequest("Hero not found.");
+
+            return Ok(hero);
         }
 
         [HttpPost]
